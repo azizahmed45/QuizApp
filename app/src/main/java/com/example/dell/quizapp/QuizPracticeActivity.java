c102eb9a6da628c7bec5adb467db9fe93cbd2dcf
Dedar <thededar@gmail.com>
2018-07-17 22:50:21 +0600

    Little bit change

--- app/src/main/java/com/example/dell/quizapp/QuizPracticeActivity.java
+++ app/src/main/java/com/example/dell/quizapp/QuizPracticeActivity.java
@@ -43,7 +43,7 @@ public class QuizPracticeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base_question_page);
+        setContentView(R.layout.activity_quiz_practice);
 
         assignViews();
         progressBar.setVisibility(View.VISIBLE);
